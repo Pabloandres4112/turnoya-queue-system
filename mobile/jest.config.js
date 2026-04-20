6b2936407dd487ccb65d0071eb7753b19e0888a0
Pabloandres4112 <pabloandres4112@gmail.com>
2026-04-19 21:07:18 -0500

    feat: agregar patrones de ignorancia de transformación en la configuración de Jest

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ['./jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(.pnpm/)?' +
+      '(' +
+      '@react-native(-community)?|' +
+      'react-native|' +
+      '@react-navigation|' +
+      'react-native-screens|' +
+      'react-native-gesture-handler|' +
+      'react-native-safe-area-context' +
+      ')' +
+      ')',
+  ],
 };
